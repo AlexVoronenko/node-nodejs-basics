8df090e89130537509ed05a939923d9503e78bb4
Alexander Voronenko <796546@gmail.com>
2022-11-29 20:16:24 +0300

    feat: add fs/copy and fs/create solutions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { join } from 'path';
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", 'fresh.txt');
+  const data = "I am fresh and young";
+  const promise = writeFile(filePath, data, { flag: 'wx' });
+
+  try {
+    await promise;
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
