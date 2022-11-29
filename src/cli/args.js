976648527483328397f1113ef0361cdc1f43c86d
Alexander Voronenko <796546@gmail.com>
2022-11-29 22:39:49 +0300

    feat: add cli/argv solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  let result = [];
+
+  argv.forEach((value, index, array) => {
+    if (value.startsWith("--") && (index < array.length)) {
+      result.push(`${value.slice(2)} is ${array[index + 1]}`);
+    }
+  });
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
