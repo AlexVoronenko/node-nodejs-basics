8b7d9f2621b55b27d93f7435c69ebe1ac1c39344
Alexander Voronenko <796546@gmail.com>
2022-11-29 21:12:19 +0300

    feat: comlete all tasks for fs/files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { join } from 'path';
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderName = join(__dirname, "files");
+
+  try {
+    const list = await readdir(folderName, { withFileTypes: true });
+
+    for (const file of list) {
+      if (file.isFile()) console.log(file.name);
+    }
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
+
 };
 
 await list();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { join } from 'path';
+import { rename as renameFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  const oldFile = join(__dirname, "files", "wrongFilename.txt");
+  const newFile = join(__dirname, "files", "properFilename.md");
+
+  try {
+    await renameFile(oldFile, newFile);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
+
 };
 
 await rename();
